11ab3e0eac33e5c91ee21fe6f583ca1185969900
Peter Iordanov <iordano2@illinois.edu>
2015-11-24 17:14:44 -0600

    added basic PlayerModule
    placeholder for PlayerModule

---
+++ Classes/Player.h
@@ -0,0 +1,20 @@
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+
+#include <list>//might not need this
+#include <map>
+class Player
+{
+	public:
+		Player();
+
+		//Player(Color c) so we can customize
+		
+
+	private:
+		//list of units
+		//list of owned cities
+		//player color and other metadata
+
+};
+#endif //__PLAYER_H__
